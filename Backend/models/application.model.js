b1e15654ec9c6180b69b10fc7b36cfe016d979be
Lalit kumar Rout <routlalit172@gmail.com>
2024-08-04 01:49:34 +0530

    application

--- Backend/models/application.model.js
+++ Backend/models/application.model.js
@@ -1,22 +1,20 @@
 import mongoose from "mongoose";
 
-const applicationSchema=new mongoose.Schema({
+const applicationSchema = new mongoose.Schema({
     job:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:'job',
+        ref:'Job',
         required:true
     },
     applicant:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
         required:true
-
     },
     status:{
         type:String,
-        enum:['pending','accepted','rejected'],
+        enum:['pending', 'accepted', 'rejected'],
         default:'pending'
-
     }
 },{timestamps:true});
-export const Application=mongoose.model('Application',applicationSchema);
+export const Application  = mongoose.model("Application", applicationSchema);
