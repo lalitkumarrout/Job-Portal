1d09af39c9e761a2017220463b6ffaa9784712d3
Lalit kumar Rout <routlalit172@gmail.com>
2024-08-10 19:45:38 +0530

    login and logout page

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 export const register = async (req, res) => {
   try {
     const { fullname, email, phoneNumber, password, role } = req.body;
+
+    console.log(fullname, email, phoneNumber, password, role)
     if (!fullname || !email || !phoneNumber || !password || !role) {
       return res
         .status(400)
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { login, logout, register, updateProfile } from '../controllers/user.controller.js';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
+import { singleupload } from '../middlewares/multer.js';
 
 const  router=express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleupload,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout)
 router.route("/profile/update").post(isAuthenticated,updateProfile);
